141fc950d43c2b13eb3b4214566e5ca4eba9b7a3
Dolly Kapoor <Dolly Kapoor@LAPTOP-RK32T7HV>
2018-08-05 19:44:35 +0530

    basic

---
+++ dolly/src/main/java/com/niit/Product.java
@@ -0,0 +1,37 @@
+package com.niit;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int pid;
+	private String pname;
+	private int pcost;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public int getPcost() {
+		return pcost;
+	}
+	public void setPcost(int pcost) {
+		this.pcost = pcost;
+	}
+	
+	
+	
+	
+	
+}
